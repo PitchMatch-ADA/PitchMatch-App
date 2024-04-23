b8644055f86743d800b9871c2c0bbd08663db970
darren thiorres <darrenthiores@gmail.com>
2024-04-23 18:46:48 +0700

    + test update

--- PitchMatch/ContentView.swift
+++ PitchMatch/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "music.note")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Musik!")
         }
         .padding()
     }
