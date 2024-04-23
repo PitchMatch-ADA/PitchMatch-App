6c2e8f9186e7f8f60a0564995f02249c34c9f1a3
darren thiorres <darrenthiores@gmail.com>
2024-04-23 17:41:51 +0700

    Initial Commit

---
+++ PitchMatch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PitchMatch
+//
+//  Created by Darren Thiores on 23/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PitchMatch/PitchMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PitchMatchApp.swift
+//  PitchMatch
+//
+//  Created by Darren Thiores on 23/04/24.
+//
+
+import SwiftUI
+
+@main
+struct PitchMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
