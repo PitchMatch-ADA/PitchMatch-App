94148b4c35c157be6c89745f3731335652a72b3f
darren thiorres <darrenthiores@gmail.com>
2024-04-26 23:13:32 +0700

    + add folder

---
+++ PitchMatch/View/Record/RecordView.swift
@@ -0,0 +1,18 @@
+//
+//  RecordView.swift
+//  PitchMatch
+//
+//  Created by Darren Thiores on 26/04/24.
+//
+
+import SwiftUI
+
+struct RecordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecordView()
+}
