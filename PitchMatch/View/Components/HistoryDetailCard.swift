f1d71a44175f8e41cb60428ef844df9ad59b1a7d
NJuGau <nathanael.gauthama@binus.ac.id>
2024-04-28 14:15:24 +0700

    Fix History detail card, add logo

--- PitchMatch/View/Components/HistoryDetailCard.swift
+++ PitchMatch/View/Components/HistoryDetailCard.swift
@@ -10,34 +10,46 @@ import SwiftUI
 struct HistoryDetailCard: View {
     //    @State var record: History
     @State var song: Song //TODO: nnti ganti History just wanna test some things
+    var color: Color?
+    var proxy: GeometryProxy
     var body: some View {
-        HStack{
-            Text("100%")
-                .foregroundStyle(Color.yellowMain)
-                .font(.title3)
-                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-            //            RecorderDisplay(backgroundRatios: song.powerRatios.map{ ratio in
-            //                Double(truncating: ratio as NSNumber)
-            //            },
-            //                            powerRatios: song.powerRatios.map{ ratio in
-            //                Double(truncating: ratio as NSNumber)
-            //            },
-            //                            color: Color.yellowMain)
-            CircleButton(iconName: "play.fill"){
+        
+            HStack(){
+                Text("100%")
+                    .foregroundStyle(Color.yellowMain)
+                    .font(.title3)
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                //            RecorderDisplay(backgroundRatios: song.powerRatios.map{ ratio in
+                //                Double(truncating: ratio as NSNumber)
+                //            },
+                //                            powerRatios: song.powerRatios.map{ ratio in
+                //                Double(truncating: ratio as NSNumber)
+                //            },
+                //                            color: Color.yellowMain)
                 
+                RecorderDisplay(backgroundRatios: song.powerRatios.map{ ratio in
+                    Double(truncating: ratio as NSNumber)
+                }, powerRatios: song.powerRatios.map{ ratio in
+                    Double(truncating: ratio as NSNumber)
+                }, color: color ?? Color.yellowMain, proxy: proxy, height: 25)
+                CircleButton(iconName: "play.fill"){
+                    
+                }
+                .scaleEffect(0.5)
+                .frame(width: 50, height: 50)
             }
-            .scaleEffect(0.5)
-            .frame(width: 50, height: 50)
+            .frame(height: 100)
+            .padding(5)
+            .background(Color.bg)
+            .clipShape(.rect(cornerSize: CGSize(width: 10, height: 10)))
         }
-        .frame(height: 100)
-        .padding(5)
-        .background(Color.bg)
-        .clipShape(.rect(cornerSize: CGSize(width: 10, height: 10)))
-    }
 }
 
 #Preview {
-    HistoryDetailCard(
-        song: Song.getSongs()[1]
-    )
+    GeometryReader { proxy in
+        HistoryDetailCard(
+            song: Song.getSongs()[1],
+            proxy: proxy
+        )
+    }
 }
--- PitchMatch/View/HistoryDetail/HistoryDetailView.swift
+++ PitchMatch/View/HistoryDetail/HistoryDetailView.swift
@@ -13,36 +13,38 @@ struct HistoryDetailView: View {
     @State var selectedSong: Int = 0
     
     var body: some View {
-        VStack{
-            CircularProgressBar(progress: 10, singer: singer ?? Singer.getSingers()[0], barColor: singer?.getShadeColor() ?? .yellowShade4)
-            Spacer()
-            TabView(selection: $selectedSong,
-                    content:  {
-                ForEach(Singer.getSingers()) { singer in
-                    ScrollView{
-                        ForEach(singer.clips){ song in
-                            HistoryDetailCard(song: song)
+        GeometryReader { proxy in
+            VStack{
+                CircularProgressBar(progress: 10, singer: singer ?? Singer.getSingers()[0], barColor: singer?.getShadeColor() ?? .yellowShade4)
+                Spacer()
+                TabView(selection: $selectedSong,
+                        content:  {
+                    ForEach(Singer.getSingers()) { singer in
+                        ScrollView{
+                            ForEach(singer.clips){ song in
+                                HistoryDetailCard(song: song, proxy: proxy)
+                            }
                         }
                     }
-                }
-                .padding()
-            })
-            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
-            .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
-            //TODO: Masih trial pake Song, jangan lupa ganti
-        }
-        .padding(.vertical, 64)
-        .background(
-            LinearGradient(
-                colors: [
-                    singer?.getShadeColor() ?? .yellowShade4,
-                    singer?.getTintColor() ?? .yellowTint4,
-                    singer?.getShadeColor() ?? .yellowShade4
-                ],
-                startPoint: .top,
-                endPoint: .bottom
+                    .padding()
+                })
+                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
+                .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+                //TODO: Masih trial pake Song, jangan lupa ganti
+            }
+            .padding(.vertical, 64)
+            .background(
+                LinearGradient(
+                    colors: [
+                        singer?.getShadeColor() ?? .yellowShade4,
+                        singer?.getTintColor() ?? .yellowTint4,
+                        singer?.getShadeColor() ?? .yellowShade4
+                    ],
+                    startPoint: .top,
+                    endPoint: .bottom
+                )
             )
-        )
+        }
     }
 }
 
