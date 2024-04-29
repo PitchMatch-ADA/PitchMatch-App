bef644cab6887ac04cf1c117d614619b71e5d68d
darren thiorres <darrenthiores@gmail.com>
2024-04-29 19:56:53 +0700

    + fix lyric accuracy + fix home songs

--- PitchMatch/Model/Record/VoiceToTextParser.swift
+++ PitchMatch/Model/Record/VoiceToTextParser.swift
@@ -62,7 +62,7 @@ class VoiceToTextParser: ObservableObject {
             try audioSession?.setCategory(
                 .playAndRecord,
                 mode: .spokenAudio,
-                options: .defaultToSpeaker
+                options: .duckOthers
             )
             
             try audioSession?.setActive(
