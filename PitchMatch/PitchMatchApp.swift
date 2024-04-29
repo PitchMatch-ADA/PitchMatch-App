e79aabf470ea356d052648e3587c05839092048c
darren thiorres <darrenthiores@gmail.com>
2024-04-30 00:41:17 +0700

    + enable swipe to back

--- PitchMatch/PitchMatchApp.swift
+++ PitchMatch/PitchMatchApp.swift
@@ -45,3 +45,14 @@ struct PitchMatchApp: App {
         .modelContainer(for: [History.self])
     }
 }
+
+extension UINavigationController: UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}
